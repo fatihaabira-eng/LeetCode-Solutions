550572dd7ebe082412217fb3885b024e27074a06
fatiha <fatihaabira05@gmail.com>
2024-12-23 20:40:15 +0100

    Revert "Initial commit for easy solutions"
    This reverts commit cea74d0b56a0bf5bad1a03787e0e53ab1f44b9c2.

--- candies_problem.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-def kidsWithCandies(candies: List[int], extraCandies: int) -> List[bool]:
-    bigNumber = max(candies)
-    return [candy + extraCandies >= bigNumber for candy in candies]
-
-print(kidsWithCandies([2,3,5,1,3],3)) #Output: [true,true,true,false,true] 
-print(kidsWithCandies( [4,2,1,1,2],1)) #Output: [true,false,false,false,false] 
